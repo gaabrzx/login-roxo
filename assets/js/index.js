4503a631c2008078e6649c9e41e011e4b935b58a
Gabriel Trajano <slashnebulous02@gmail.com>
2022-11-23 09:38:59 -0300

    second commit

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+const link = document.getElementById("signup-link");
+
+link.addEventListener("click", function () {
+  window.location = "signup.html";
+});
---
+++ assets/js/signup.js
@@ -0,0 +1,5 @@
+const link = document.getElementById("link-redirect");
+
+link.addEventListener("click", function () {
+  window.location = "index.html";
+});
